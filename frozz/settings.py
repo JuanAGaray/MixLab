38d51799f046082aa5c9082f7b31b405ad334d39
JuanAGaray <130544436+JuanAGaray@users.noreply.github.com>
2026-02-11 13:35:57 -0500

    Fixed Settings

--- frozz/settings.py
+++ frozz/settings.py
@@ -17,12 +17,12 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-change-this-in-production-!@#$%^&*()'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 # Application definition
